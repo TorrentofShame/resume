dd1ca82b20d0085912a8a6f8550001cd8ec43d2b
Simon Weizman <contact@simon.weizman.us>
2021-04-29 09:26:29 -0400

    Added blank spec file for resume.lua so I can add testing to it

--- resume.lua
+++ resume.lua
@@ -1,53 +1,53 @@
---config = config or require("config")
+config = config or require("config")
+tex = tex or { sprint=print }
 
 local f = {
   --[[ Load Profile data from config ]]
   profile = function()
     prof = ""
     for k, v in pairs(config.profile) do
-      prof = prof.."\\re"..k.."{"..v.."}\n"
+      prof = prof .. "\\re" .. k .. "{" .. v .. "}\n"
     end
     return prof
   end,
   --[[ Load Experience data from config ]]
   experience = function()
-
     entry = ""
-    for _,e in ipairs(config.experience) do
-      period = e.startDate.." -- "..(e.endDate or "Current")
-      entry = entry .. "\\expentry{"..e.jobTitle.."}{"..e.organization.."}{"..period.."}{"..e.location.."}{\\begin{itemize}"
+    for _, e in ipairs(config.experience) do
+      period = e.startDate .. " -- " .. (e.endDate or "Current")
+      entry =
+        entry ..
+        "\\expentry{" ..
+          e.jobTitle .. "}{" .. e.organization .. "}{" .. period .. "}{" .. e.location .. "}{\\begin{itemize}"
       desc = ""
 
-      for i=1,#e.description do
-        desc = desc .. "\\item "..e.description[i].."\n"
+      for i = 1, #e.description do
+        desc = desc .. "\\item " .. e.description[i] .. "\n"
       end
 
-      entry = entry..desc.."\\end{itemize}\\leavevmode}"
+      entry = entry .. desc .. "\\end{itemize}\\leavevmode}"
     end
-    
+
     return entry
   end,
   --[[ Load Project data from config ]]
   projects = function()
-
     entry = ""
-    for _,p in ipairs(config.projects) do
-      period = p.startDate.." -- "..(p.endDate or "Current")
-      entry = entry .. "\\projectentry{"..p.name.."}{"..period.."}{"..p.shortDescription.."}\\\\"
+    for _, p in ipairs(config.projects) do
+      period = p.startDate .. " -- " .. (p.endDate or "Current")
+      entry = entry .. "\\projectentry{" .. p.name .. "}{" .. period .. "}{" .. p.shortDescription .. "}\\\\"
     end
 
     return entry
   end,
   --[[ Load Skill data from config ]]
   skills = function()
-
     entry = ""
-    for _,skills in ipairs(config.skills) do
+    for _, skills in ipairs(config.skills) do
+      entry = entry .. "\\begin{skillcat}{" .. skills.categoryName:gsub("(&)", [[\%1]]) .. "}"
 
-      entry = entry .. "\\begin{skillcat}{"..skills.categoryName:gsub("(&)", [[\%1]]).."}"
-
-      for _,skill in ipairs(skills.items) do
-        entry = entry .. "\\skill{"..skill.."}"
+      for _, skill in ipairs(skills.items) do
+        entry = entry .. "\\skill{" .. skill .. "}"
       end
 
       entry = entry .. "\\end{skillcat}\\\\"
@@ -57,21 +57,23 @@ local f = {
   end,
   --[[ Load Education data from config ]]
   education = function()
-
     entry = ""
-    for _,edu in ipairs(config.education) do
-      period = edu.startDate.." -- "..edu.endDate
-      entry = entry .. "\\eduentry{"..edu.degree.."}{"..period.."}{"..edu.institution.."}"
+    for _, edu in ipairs(config.education) do
+      period = edu.startDate .. " -- " .. edu.endDate
+      entry = entry .. "\\eduentry{" .. edu.degree .. "}{" .. period .. "}{" .. edu.institution .. "}"
     end
 
     return entry
   end,
   --[[ Load Certification data from config ]]
   certifications = function()
-
     entry = ""
     for _, cert in ipairs(config.certifications) do
-      entry = entry .. "\\certentry{"..cert.name.."}{"..cert.issuer.."}{"..cert.issueDate.."}{"..(cert.expireDate or "").."}{"..cert.logo.."}"
+      entry =
+        entry ..
+        "\\certentry{" ..
+          cert.name ..
+            "}{" .. cert.issuer .. "}{" .. cert.issueDate .. "}{" .. (cert.expireDate or "") .. "}{" .. cert.logo .. "}"
     end
 
     return entry
@@ -80,10 +82,15 @@ local f = {
 
 --[[ Function "wrapper" so I can just simply return what I need to write to the tex document ]]
 local wrapper = {}
-setmetatable(wrapper, {
-  __index = function(table, key)
-    return function(...) tex.sprint(f[key](...)) end
-  end
-})
+setmetatable(
+  wrapper,
+  {
+    __index = function(table, key)
+      return function(...)
+        tex.sprint(f[key](...))
+      end
+    end
+  }
+)
 
 return wrapper
---
+++ resume_spec.lua
@@ -0,0 +1,25 @@
+local f = require("resume")
+config = require("config")
+
+describe("profile", function ()
+
+    it("not empty", function ()
+        assert.has_no.errors(f.profile)
+    end)
+
+end)
+
+describe("experience", function ()
+end)
+
+describe("projects", function ()
+end)
+
+describe("skills", function ()
+end)
+
+describe("education", function ()
+end)
+
+describe("certifications", function ()
+end)
