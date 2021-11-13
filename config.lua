39ad57aac5b156efa096cfd694eaed1f2c4d3e54
Simon Weizman <contact@simon.weizman.us>
2021-11-12 20:28:05 -0500

    Updated some stuff, made some fixes, and move some stuff around

--- config.lua
+++ config.lua
@@ -2,13 +2,16 @@ local config = {
   profile = {
     logo = "logo",
     name = "Simon Weizman",
-    headline = "Full-Stack Developer \\& CS Student at UCF",
+    headline = "Computer Science Student at University of Central Florida | SWE Intern at SavvySuit",
     email = "contact@simon.weizman.us",
-    website = "simon.weizman.us",
+    website = "simon.weizman.dev",
     git = "gitlab.com/torrentofshame",
     location = "Orlando, Florida, US"
   },
-  summary = nil, -- nil causes lipsum[1][1-2]
+  summary = [[
+        I'm a passionate, resourceful, and self-taught developer and an undergraduate student of Computer Science at UCF.
+        I decided to take on freelance development as a way to further my abilities and solve niche problems that I wouldn't be able to find elsewhere.
+  ]], -- nil causes lipsum[1][1-2]
   experience = {
     {
       jobTitle = "Software Engineering Intern",
@@ -17,22 +20,53 @@ local config = {
       endDate = nil,
       location = "Orlando, FL",
       description = {
-          "Working in a team of 5 people (1 other intern) to create and develop software."
+          --"Working in a team of 5 people (1 other intern) to create and develop software.",
+          -- Following is copy from Weiyi resume
+          "Work on an offline-first, ADA compliant progressive web app that assists disabilities living in locatinos with poor internet",
+          "Build a form structure in a yaml config file, create dynamic forms using Formik and JavaScript, which take in applicants' input and generate new fields accordingly if needed, and ensure the reusability of code when the structure of form changes",
+          "Manipulate local data, enable offline and cloud synchronization using Amplify DataStore in the frontend",
+          "Implement data conflict resolution, versioning and journaling using AWS AppSync with DynamoDB in the backend"
+          -- End of Copy
       }
+    },
+    {
+        jobTitle = "Freelance Developer",
+        organization = "Upwork",
+        startDate = "Mar 2021",
+        endDate = nil,
+        location = "Remote/Virtual",
+        description = {
+            "Working closely with clients to create custom scripts and modify pre-existing code."
+        }
+    },
+    {
+        jobTitle = "DevOps Lead",
+        organization = "Knight Hacks",
+        startDate = "Aug 2021",
+        endDate = "Nov 2021",
+        location = "Remote/Virtual",
+        description = {
+            "Deployed and managed our applications running on a combination of Kubernetes, Azure, and Cloudflare.",
+            "Built, maintained, and scaled infrastructure for production and staging environments.",
+            "Used GitHub Actions to automate testing and deployment to production and staging environments.",
+            "Oversaw the implementation of automated performance measurement reporting and error monitoring.",
+            "Developed automated pipelines for creating and deploying new releases using semver and GitHub Actions."
+        }
     },
     {
       jobTitle = "Back End Developer",
       organization = "Knight Hacks",
       startDate = "Feb 2021",
-      endDate = nil,
+      endDate = "Nov 2021",
       location = "Remote/Virtual",
       description = {
-        "Working in a team of 5 people using Flask and MongoDB to create tools for student hackathons.",
-        "Using Agile Methodology in the team.",
-        "Collaborate with front end and mobile teams to ensure the quality of the final product.",
-        "Design and maintained unit tests in collaboration with team members to ensure that the code functions properly.",
-        --"Architected the initial boilerplate code.",
-        "Assisted in deployment using GitHub Actions, Kubernetes, and Nginx."
+        "Worked in a team of 6 people using Flask and MongoDB to create tools for student hackathons.",
+        "Used Agile Methodology in the team.",
+        "Collaborated with front end and mobile teams to ensure the quality of the final product.",
+        "Designed and maintained unit tests in collaboration with team members to ensure code quality.",
+        "Architected the initial boilerplate code.",
+        "Assisted in deployment using GitHub Actions, Kubernetes, and Nginx.",
+        "Built and deployed various hotfixes and last-minute changes to the backend."
       }
     },
     {
@@ -45,10 +79,10 @@ local config = {
         "Developed a player management system utilizing an SQL database which enhanced the player experience and assisted in managing the player hierarchy.",
         "Designed custom UI that integrated existing features to assist players and staff in managing characters and the server.",
         "Cooperated with staff and players to increase player retention during down-time by 50\\%.",
-        --"Documented the steps necessary to configure the different features on the server.",
+        "Documented the steps necessary to configure the different features on the server.",
         "Maintained and managed the server's code and database."
       }
-    }--[[,
+    },
     {
       jobTitle = "Summer Camp Counselor",
       organization = "Peg Camp",
@@ -60,14 +94,14 @@ local config = {
         "Taught multiple computer programs to children at the beginning, intermediate, and advanced levels, including: Python and Tinker CAD.",
         "Supported campers through the learning process ensuring a fun and successful experience."
       }
-    }--]]
+    }
   },
   projects = {
     {
-      name = "Verum Interactive Starmap",
-      startDate = "Dec 2020",
-      endDate = "Jan 2021",
-      shortDescription = "An interactive starmap for Arcadum's world of Verum. Art is by Yowto with permission."
+      name = "LuaLaTeX Resume",
+      startDate = "Nov 2020",
+      endDate = nil,
+      shortDescription = "My resume that I made in LuaLaTeX, that is generated from information saved in a config file."
     },
     {
       name = "Personal Website",
@@ -80,7 +114,25 @@ local config = {
       startDate = "Jul 2020",
       endDate = nil,
       shortDescription = "A custom Webmail client created with Node.js, Express.js, and Pug.js"
-    }
+    },
+    {
+      name = "Back-end for Knight Hacks",
+      startDate = "Feb 2021",
+      endDate = "Nov 2021",
+      shortDescription = "The back-end for Knight Hacks, built with Flask and deployed on AKS."
+    },
+    {
+        name = "Personal Homelab Server",
+        startDate = "Mar 2018",
+        endDate = "Jul 2020",
+        shortDescription = "I owned, maintained, and administered an old Dell Poweredge R710 that I used to learn about virtualization, DevOps and server management"
+    },
+    {
+        name = "Why don't people format their numbers browser extension",
+        startDate = "Sep 2020",
+        endDate = "Sep 2020",
+        shortDescription = "A very small and simple Google Chrome browser extension using JavaScript to help me read large, especially unformatted numbers."
+    },
   },
   skills = {
     {
@@ -91,16 +143,19 @@ local config = {
         "SASS",
         "LESS",
         "JavaScript",
+        "TypeScript",
         "Python",
         "Lua",
         "LaTeX",
-        "C"
+        "C",
+        "Java"
       }
     },
     {
       categoryName = "Frontend",
       items = {
         "React.js",
+        "React Native",
         "Bootstrap",
         "jQuery",
         "Webpack",
@@ -128,7 +183,8 @@ local config = {
         "PostgreSQL",
         "MariaDB",
         "MySQL",
-        "SQLite"
+        "SQLite",
+        "DynamoDB"
       }
     },
     {
@@ -144,7 +200,9 @@ local config = {
         "Grafana",
         "Digital Ocean",
         "Kubernetes",
-        "AWS"
+        "AWS",
+        "Linux",
+        "Vim"
       }
     }
   },
@@ -153,7 +211,7 @@ local config = {
       degree = "Computer Science BS",
       institution = "University of Central Florida",
       startDate = "Jul 2020",
-      endDate = "May 2024"
+      endDate = "Dec 2023"
     },
     {
       degree = "High School Diploma",
